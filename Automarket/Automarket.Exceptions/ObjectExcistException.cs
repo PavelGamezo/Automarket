f8982f23d6afc4511c5c6b89bbf225def058a8a0
PavelGamezo <pashasblv@gmail.com>
2023-03-17 01:33:13 +0300

    Add exceptions classes on project

---
+++ Automarket/Automarket.Exceptions/ObjectExcistException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automarket.Exceptions
+{
+    public class ObjectExcistException : Exception
+    {
+        public ObjectExcistException(string objectName) : base($"{objectName} was found!")
+        {
+
+        }
+    }
+}

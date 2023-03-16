ccb8d1e9dbb4def37a7283088492c7f4b556732f
PavelGamezo <pashasblv@gmail.com>
2023-03-17 01:28:44 +0300

    Add entities and enums

---
+++ Automarket/Automarket.Entities/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using Automarket.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automarket.Entities.Entities
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Nickname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+
+        public List<Car>? Cars { get; set; }
+    }
+}
